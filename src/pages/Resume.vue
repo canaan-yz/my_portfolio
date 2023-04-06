284e922a9bafacd309fbf5577392bc329a9c2929
Zheng Yang <canaanyangzheng@gmail.com>
2023-04-06 21:27:33 +0800

    Resume.vue resume content almost complete

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -15,10 +15,11 @@
       <!--grid-rows-cv-layout tailwind.config.cjs -->
       <article id="A4" class="mx-auto w-[210mm] h-[297mm] xl:scale-150 origin-top bg-white shadow-md px-14 py-6
                       text-neutral-700 text-xs font-light leading-4 text-left subpixel-antialiased
-                      grid grid-flow-col grid-rows-cv-layout grid-cols-1 gap-y-0.5">
-        <section name="header" class="">
+                      grid grid-flow-col grid-rows-24 grid-cols-3 gap-2">
+
+        <section name="header" class="row-span-2 col-span-full">
           <h1 class="text-2xl font-semibold text-neutral-900">Zheng Yang</h1>
-          <h2 class="">Python web engineer/Full-stack developer/Programming teacher</h2>
+          <h2 class="">Python Web Engineer/Full-Stack Developer/Programming Teacher</h2>
           <p class="">
             <AtSymbolIcon class="w-4 inline-block"/>canaanyangzheng@gmail.com&nbsp;
             <LinkIcon class="w-4 inline-block"/>www.1owo.com&nbsp;
@@ -27,60 +28,232 @@
           </p>
         </section>
 
-        <section name="middle" class="bg-amber-200 col-span-2
-                        grid grid-flow-col grid-rows-24 grid-cols-cv-layout gap-y-2">
-          <div name="summery" class="bg-blue-200 row-span-3">
-            <div>
-                <h2 class="text-sm font-semibold text-neutral-700">SUMMERY</h2>
-                <p class="">
-                  Major in Python Web Backend development. Full-stack developer, from software requirements analysis, to prototype/UI, frontend, backend, and final deployment.
-                  Teaching at programing education company and universities.
-                </p>
-            </div>
+        <section name="summery" class="row-span-2 col-span-2 bg-blue-200">
+          <div>
+            <h2 class="text-sm font-semibold text-neutral-700">Summery</h2>
+            <p class="">
+              Major in Python Web Backend development. Full-stack developer, from software demand analysis, to prototype/UI, frontend, backend, and final deployment.
+              Teaching at programing education company and universities.
+            </p>
           </div>
+        </section>
 
-          <div name="experience" class="bg-blue-50 border-2 row-span-5">
-            <h2 class="">EXPERIENCE</h2>
-              <ol class="relative border-l-2 border-neutral-500">
-                <li class="mb-4 ml-2">
-                    <div class="absolute w-3 h-3 rounded-full bg-neutral-500 mt-1.5 -left-[0.4rem] border-2 border-white"></div>
-                    <time class="mb-1 text-neutral-500">2022</time>
-                    <h3 class="">Application UI code in Tailwind CSS</h3>
-                    <p class="mb-4">
-                      Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar,
-                      and pre-order E-commerce & Marketing pages.
+        <section name="experience" class="row-span-20 col-span-2 group bg-blue-50">
+          <h2 class="">Experience</h2>
+            <ol name="Teaching" class="">
+              <li class="">
+                <div class=""></div>
+                <time class="">2022</time>
+                <h3 class="ml-4">Beijing XueJiaAo Education</h3><span class="ml-4">Programming Teacher</span>
+                <h4 class="">Company Introduction:</h4>
+                <p class="">
+                  The unique authorization of Oracle Education in China, operate IT training and school-enterprise cooperation teaching.
+                </p>
+                <h4>Tech Stack:</h4>
+                <p class="">
+                  Python, Django, Frontend. Computer foundation courses etc.
+                </p>
+                <h4>My responsibility:</h4>
+                <p class="">
+                  •"Python from Zero Foundation to Employment in 20 Weeks" training course.<br/>
+                  •"Frontend from Zero Foundation to Employment in 20 Weeks" training course(first 70% syllabus).<br/>
+                  •"Python Django Video Project" training for new teachers in universities.<br/>
+                  •"Python Crawls and Visualization" practical training in Henan University,<br/>
+                  Zhengzhou College of Aeronautics and Shenyang University of Technology.<br/>
+                  •"Python Foundation Coding" required courses, "Software Testing", "Server Virtualization" elective courses in
+                  Zhengzhou Tourism Vocational College.<br/>
+                  •"Raspberrypi Air Quality Detector" propaganda course in Tiktok.<br/>
+                  •"H5 Page Production Competition" in Shanxi Agricultural College.<br/>
+                  Sum up: Teaching plan development, Teaching(mainly python on campus or online), Students Q&A,
+                  Publicity classes, Students assessment.
+                </p>
+              </li>
+            </ol>
+
+            <ol name="LongQi" class="">
+                <li class="">
+                    <div class=""></div>
+                    <time class="">2017</time>
+                    <h3 class="ml-4">LongQi Wig</h3><span class="ml-4">ERP Odoo Developer</span>
+                    <h4 class="">Company Introduction:</h4>
+                    <p class="">
+                        Xuchang, China has the advantage of the lowest price of wig in the world. Through foreign trade,
+                        the company has grown rapidly. Opened more than 20 stores including self-operated malls,
+                        eBay stores, Amazon stores, and AliExpress stores, and placed advertisements on Google and YouTube.
+                        No matter where you buy wigs online, it's the same boss behind.
+                    </p>
+                    <h4>Tech Stack:</h4>
+                    <p class="">
+                      Odoo, Postgresql, python scenario, E-commerce platform API, Thermal printer
+                    </p>
+                    <h4>My responsibility:</h4>
+                    <p class="">
+                        Due to the large number of stores and the rapid growth of orders, it was difficult for the sales staff to handle.<br/>
+                        I participate in development of Odoo OA to unify the business process.
+                        Request the API of each e-commerce platform to pull the order information and extract the public structure and store it in the database,
+                        then sales team process orders, and then orders go to the finance department to generate bills, finally go to the logistics department to generate the waybill.<br/>
                     </p>
                 </li>
-              </ol>
-          </div>
+            </ol>
+
+            <ol name="ChuangHeng-TBKT" class="">
+                <li class="">
+                    <div class=""></div>
+                    <time class="">2015</time>
+                    <h3 class="ml-4">Synchronized Classroom</h3><span class="ml-4">Python Backend Developer</span>
+                    <h4 class="">Company Introduction:</h4>
+                    <p class="">
+                      A primary education app, including subjects such as Chinese, mathematics, and English.
+                      Benefits from collaborating with China Mobile and schools in Henan, during its heyday,
+                      there were 200k active users who were online for more than 2hr a day.
+                    </p>
+                    <h4>Tech Stack:</h4>
+                    <p class="">
+                        Django, MySQL, Vue, Docker. Iterative incremental development.
+                    </p>
+                    <h4>My responsibility:</h4>
+                    <p class="">
+                      Mainly responsible for the functional development of the primary school English module,
+                      Collaborate with PM,editors,front-end developers and testers colleagues.<br/>
+                      I developed APIs about English teaching material management, test paper, school competition activities, teacher end management etc.<br/>
+                    </p>
+                </li>
+            </ol>
+
+            <ol name="UI" class="hidden group-hover:block">
+                <li class="">
+                    <div class=""></div>
+                    <time class="">2014</time>
+                    <h3 class="ml-4">UI related</h3><span class="ml-4">UI Designer</span>
+                    <h4 class="">Company Introduction:</h4>
+                    <p class="">
+                      (I hidde this work to avoid that you may be confused that a backend coder did UI work.)<br/>
+                      Alcohol Delivery app and some part-time works.
+                    </p>
+                    <h4>Tech Stack:</h4>
+                    <p class="">
+                      Photoshop
+                    </p>
+                    <h4>My responsibility:</h4>
+                    <p class="">
+                      In 2014, the Chinese internet was in a rapid development trend.
+                      In addition to paying attention to the interface for its functions, the market demands a large number of UI talents.<br/>
+                      Due to the difficulty of developing SAP+Weaver in my previous job, I temporarily gave up programming and self-taught UI and UX.
+                      At that time, there is not even find one or two UI tutorial books. I learnt succulently, and ultimately found a job.<br/>
+                      I restructured the company's Alcohol Delivery app interface, and adapted various interfaces in conjunction with the Android and IOS colleague.
+                      One years later, the market began to saturate and there were few high-end UI positions in my city.
+                      I felt that there would be a bottleneck if I didn't have a major in art, so I gave up on continuing UI work.<br/>
+                      But the aesthetic ability remains.
+                    </p>
+                </li>
+            </ol>
+            <!--hidde some unimportant experiences cos limited space-->
+            <!--https://tailwindcss.com/docs/hover-focus-and-other-states#styling-based-on-parent-state-->
+            <ol name="Dennis" class="hidden group-hover:block">
+              <li class="">
+                  <div class=""></div>
+                  <time class="">2013</time>
+                  <h3 class="ml-4">Zhengzhou Dennis Department Store Co.Ltd</h3><span class="ml-4">ERP Developer</span>
+                  <h4 class="">Company Introduction:</h4>
+                  <p class="">
+                    (I hide this work because space is limited in one page resume and I don't like the develop language this work used.)
+                    A Taiwan-funded enterprise, With an annual turnover of 20 billion in 2018, ranks 1th in Henan province and ranks 30th in the nation in retail industry.
+                  </p>
+                  <h4>Tech Stack:</h4>
+                  <p class="">
+                    SAP, Weaver OA. Secondary development. Abap, Java, Js. Walter fall development.
+                  </p>
+                  <h4>My responsibility:</h4>
+                  <p class="">
+                    Conduct demand interviews with the finance department and maintenance department key users, preliminary sorting of demand.<br/>
+                    Received a little Abap training from the SAP team of Party B and training from the Weaver team.<br/>
+                    I developed functions such as financial reimbursement forms and repair work orders.<br/>
+                  </p>
+              </li>
+            </ol>
         </section>
 
-
-
-
-<!--        </div>-->
-<!--        <div class="bg-amber-200 " name="tech-stack">-->
-<!--            <h2 class="text-xs font-semibold">tech-stack</h2>-->
-<!--        </div>-->
-<!--        <div class="" name="education&training">education&training</div>-->
-<!--        <div class="" name="language">language</div>-->
-<!--        <div class="" name="certification">certification</div>-->
-<!--        <div class="" name="avatar">avatar</div>-->
-
-        <section name="footer" class="bg-amber-200">
-            footer<br/>
-            <br/>
-            <br/>
+        <section name="avatar" class="row-span-3 col-span-1">
+          <img src="" class="rounded-full">
         </section>
+
+        <section name="skill" class="row-span-11 col-span-1 bg-blue-50">
+          <h2>Skill</h2>
+          <ul>
+            <li>
+                <h4>legend</h4>
+                <span class="_a">proficient</span><span class="_b">experienced</span><span class="_c">understand</span><span class="_z">lack energy or dislike</span>
+            </li>
+            <li>
+              <h4 class="mt-0">Production</h4>
+              <span class="_a">Demand Analysis</span><span class="_z">UI design</span><span class="_a">Doc</span><span class="_c">Project Management</span>
+            </li>
+            <li>
+              <h4 class="">Frontend</h4>
+              <span class="_a">HTML/CSS/JS/JS es5+</span><span class="_z">Bootstrap</span><span>TailwindCSS</span><span class="_c">NodeJS</span><span class="_b">VUE</span><span class="_c">React</span>
+              <span class="_b">Vite Webpack</span>
+            </li>
+            <li>
+              <h4 class="">Backend(mainly)</h4>
+              <span class="_a">Django</span><span class="_a">Flask</span><span class="_b">FastAPI</span><span class="_b">Odoo</span><span class="_c _z">tornado</span>
+              <span class="_b _z">Scrapy</span><span class="_b _z">Selenium</span><br/>
+              <span class="_a">Postgresql</span><span class="_a">MySQL</span><span class="_b _z">Oracle</span><span class="_a">SQL</span><span class="_b">MongoDB</span><span class="_c">Redis</span><br/>
+            </li>
+            <li>
+              <h4 class="">Coordination</h4>
+              <span class="_a">Git</span><span class="_b">Jira/Github Actions</span>
+            </li>
+            <li>
+              <h4 class="">Deployment</h4>
+              <span class="_b">Linux/Ubuntu</span><span class="_b">Docker</span><span class="_b">CI CD</span><span class="_b">AWS/Alibaba Cloud</span>
+            </li>
+            <li>
+                <h4 class="">Others</h4>
+                <span class="_a">RaspberryPi/sensors</span><span class="_c">Rack Server</span><span class="_c _z">Virtualization</span>
+                <span class="_b _z">Crawler</span><span class="_c _z">AI</span><span class="_c _z">Automated Maintenance</span>
+            </li>
+
+          </ul>
+
+        </section>
+
+        <section name="education" class="row-span-2 col-span-1">
+          <h2>Education</h2>
+          <h3>ZhengZhou University</h3><time class="float-right">2009~2013</time><br/>
+          <span>Computer Science</span><span class="float-right">Bachelor</span>
+        </section>
+
+        <section name="certification" class="row-span-3 col-span-1">
+          <h2>Certification</h2>
+          <ul>
+            <li class="mb-1">
+              <h3>PCPP1</h3><span class="float-right">Python Institute</span><br/>
+              <p class="">Certified Professional in Python Programming 1</p>
+            </li>
+            <li class="mb-1">
+              <h3>ACE</h3><span class="float-right">Alibaba</span><br/>
+              <p class="">Alibaba Cloud Expert Cloud Computing</p>
+            </li>
+          </ul>
+        </section>
+
+        <section name="language" class="row-span-3 col-span-1">
+          <h2>Language</h2>
+          <ul>
+            <li class="mb-1">
+                <h3>English</h3><br/>
+                <span class="">reading/writing</span><span class="float-right">influence</span><br/>
+                <span class="">listening/speaking</span><span class="float-right">limited</span>
+            </li>
+            <li class="mb-1">
+                <h3>Chinese</h3><span class="float-right">mother tongue</span><br/>
+            </li>
+          </ul>
+        </section>
+
       </article>
-
     </div>
-
-
   </Layout>
-
-
-
 </template>
 
 <script setup>
@@ -90,18 +263,77 @@ import {DevicePhoneMobileIcon, AtSymbolIcon, MapPinIcon, LinkIcon } from "@heroi
 
 </script>
 
-<style scoped>
+<style scoped lang="css">
 /*CSS nesting support https://tailwindcss.com/docs/using-with-preprocessors#nesting*/
 article{
   /*background: url("./Screenshot.png");*/
   background-size: contain;
   /*filter: opacity(0.5);*/
   & section{
+    @apply bg-blue-50;
     & h2{
-      @apply  text-sm font-medium leading-8
+      @apply  text-sm font-medium leading-8;
     }
     & h3{
-      @apply  inline-block ml-2 text-xs font-normal text-neutral-900;
+      @apply  inline-block text-xs font-medium text-neutral-700;
+    }
+    & h4{
+      @apply font-medium text-neutral-500 pl-1 border-l-2 border-neutral-300;
+    }
+    & h4:before{
+      /*content: '•';*/
+      @apply pl-0;
+    }
+
+  }
+  & section[name="experience"]{
+    & h3{
+      @apply  ml-2;
+    }
+    & p{
+        @apply pl-2;
+    }
+    & ol{
+      @apply  relative border-l-2 border-neutral-500;
+      & li{
+        /*draw time axis*/
+        @apply  mb-4 ml-2;
+        & time{
+          @apply  mb-1 text-neutral-500;
+        }
+        & >div:first-child{
+          @apply  absolute -left-[0.45rem] w-3 h-3 rounded-full bg-neutral-500 mt-1.5 border-2 border-white;
+        }
+        & >p{
+          @apply  mb-0.5;
+        }
+      }
+    }
+  }
+  & section[name="skill"]{
+    @apply font-medium;
+    & li{
+      @apply mb-1.5;
+    }
+    & span{
+      @apply  inline-block;
+    }
+    /* Skill Proficiency */
+    & span._a{
+      @apply  text-neutral-900;
+    }
+    & span._b{
+      @apply  text-neutral-600;
+    }
+    & span._c{
+      @apply  text-neutral-400;
+    }
+    & span._z{
+      @apply  line-through;
+    }
+    & span:after{
+      content: '•';
+      @apply  text-neutral-200;
     }
   }
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,10 +15,16 @@ module.exports = {
         '10': 'repeat(10, minmax(0, 1fr))',
         '12': 'repeat(12, minmax(0, 1fr))',
         '24': 'repeat(24, minmax(0, 1fr))',
-        'cv-layout': 'minmax(0, 1fr) minmax(0, 10fr) minmax(0, 1fr)',
+        'cv-layout': 'minmax(0, 1fr) minmax(0, 11fr)',
       },
       gridTemplateColumns: {
         'cv-layout': 'minmax(0, 2fr) minmax(0, 1fr)',
+      },
+      gridRow: {
+        'span-8': 'span 8 / span 8',
+        'span-10': 'span 10 / span 10',
+        'span-11': 'span 11 / span 11',
+        'span-20': 'span 20 / span 20',
       }
     },
   },
